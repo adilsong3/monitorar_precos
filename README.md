2c02b29ac4d6d01fe55fd027bfa089a7399a88cf
Adilson Gustavo <adilsons2tv@gmail.com>
2024-07-28 21:45:05 -0300

    versão 1

---
+++ README.md
@@ -0,0 +1,25 @@
+[![author](https://img.shields.io/badge/author-Adilsong-red.svg)](https://www.linkedin.com/in/adilson-gustavo-marcondes-barreto-de-souza-a74b98133/) [![](https://img.shields.io/badge/python-3.9+-blue.svg)](https://www.python.org/downloads/release/python-365/) [![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)
+
+<h1 align="center">Robo Para Acompanhar Preços</h1>
+
+<sub>*Graduando em Ciência de Dados na Uninter e Cursando Mestres da Automação*</sub>
+
+Este é um desafio do Curso Mestres da Automação desenvolvido em Python utilizando a biblioteca Selenium.
+
+<h2 align="left">Funções do Projeto:</h3>
+
+<h3 align="left">Escolha do Produto:</h3>
+
+Basicamento o usuário vai escolher que produto deseja acompanhar os preços e também se pretende mudar de mudar após 30 minutos.
+
+<h3 align="left">Extração do Nome e Preço:</h3>
+
+Extração do nome e preço do produto no site é feito utilizando a biblioteca selenium, após extrair é necessário fazer um tratamento solicitado no desafio.
+
+<h3 align="left">Criação de Planilha:</h3>
+
+Toda informação extraída deve ser armazena em uma planilha que será criada caso não exista.
+
+<h3 align="left">Agendamento:</h3>
+
+Feito um agendamento para rodar o script de 30 em 30 minutos para sempre ter na planilha os preços atualizados.
---
+++ app.py
@@ -0,0 +1,166 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+from datetime import datetime
+import openpyxl
+import os
+import schedule
+
+def iniciar_driver():
+    chrome_options = Options()
+
+    arguments = ['--lang=pt-BR', '--window-size=1200,1400', '--incognito']
+
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    caminho_padrao_para_download = 'D:\\Adilson\\Python\\2024\\monitorar_precos_destrava'
+
+    chrome_options.add_experimental_option("prefs", {
+        'download.default_directory': caminho_padrao_para_download,
+        'download.directory_upgrade': True,
+        'download.prompt_for_download': False,
+        "profile.default_content_setting_values.notifications": 2, 
+        "profile.default_content_setting_values.automatic_downloads": 1,
+    })
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+def layout():
+    choice_one = '''\n###############
+1 - Macbook
+2 - Iphone
+###############'''
+    choice_two = '''\nVocê deseja pesquisar outro produto após 30 minutos?
+1 - Sim
+2 - Não
+'''
+    return choice_one, choice_two
+
+def product_choice():
+    while True:
+        print(layout()[0])
+        choice = input('\nDigite o número da sua escolha: ').strip()
+
+        if choice == '1' or choice == 'um':
+            product_name = 'Macbook'
+            break
+        elif choice == '2' or choice == 'dois':
+            product_name = 'Iphone'
+            break
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print('Opção inválida, tente novamente!!!\n')
+            pass
+
+    while True:
+        print(f'\nProduto escolhido: {product_name}')
+        print(layout()[1])
+        choice_permanent = input('Digite o número da sua escolha: ').strip()
+        if choice_permanent == '1' or choice_permanent == 'um':
+            choice_permanent = 'Sim'
+            break
+        elif choice_permanent == '2' or choice_permanent == 'dois':
+            choice_permanent = 'Nao'
+            break
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print('Opção inválida, tente novamente!!!\n')
+            pass
+
+    url_site = f'https://www.buscape.com.br/search?q={product_name}'
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print(f'Produto escolhido: {product_name}')
+    print(f'Deseja escolher outro produto após 30 minutos: {choice_permanent}')
+
+    return url_site, choice, choice_permanent
+    
+# ○ Verificar o preço atual. # ○ Guardar o valor do preço(somente o valor numérico, não em texto)
+def extract_name_and_price(url_site, choice):
+    driver = iniciar_driver()
+    driver.get(url_site)
+    sleep(10)
+    driver.execute_script("window.scrollTo(0, 200);")
+    sleep(2)
+
+    if choice == '1' or choice == 'um':
+        xpath_product = '//*[@id="product-card-5673969::name"]'
+        product_name = driver.find_element(By.XPATH, xpath_product)
+        product_name = product_name.text
+
+        xpath_price = '//*[@id="__next"]/main/div[2]/div[7]/div[1]/div/a/div[2]/div[2]/div[2]/p'
+        price = driver.find_element(By.XPATH, xpath_price)
+        sleep(3)
+        price = price.text
+    elif choice == '2' or choice == 'dois':
+        xpath_product = '//*[@id="product-card-12474754::name"]'
+        product_name = driver.find_element(By.XPATH, xpath_product)
+        product_name = product_name.text
+
+        xpath_price = '//*[@id="__next"]/main/div[2]/div[7]/div[1]/div/a/div[2]/div[2]/div[2]/p'
+        price = driver.find_element(By.XPATH, xpath_price)
+        sleep(3)
+        price = price.text
+
+    # tratar campo preço
+    price = price.replace('R$', '')  
+    price = price.replace('.', '')  
+    price = price.replace(',', '.')
+    price = price.replace(' ', '')
+    price = float(price)
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(product_name)
+    print(price)
+
+    return driver, product_name, price
+
+def create_workbook(product_name, price, url):
+
+    filename = 'acompanhamento_de_precos.xlsx'
+    date_now = datetime.now().strftime('%Y-%m-%d')
+
+    if os.path.exists(filename):
+        workbook = openpyxl.load_workbook(filename)
+        worksheet = workbook.active
+    else:
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.active
+        worksheet.title = 'Acompanhamento de Preços'
+        worksheet.append(['Produto', 'Data atual', 'Valor', 'Link'])
+
+    # Adiciona os dados
+    worksheet.append([product_name, date_now, price, url])
+
+    # Salva a planilha
+    workbook.save(filename)
+
+    print('Dados salvos na planilha "acompanhamento_de_precos.xlsx" com sucesso!\n')
+
+
+def main(url_site, choice):
+    driver, product_name, price = extract_name_and_price(url_site, choice)
+    create_workbook(product_name, price, url_site)
+    driver.quit()
+
+    
+if __name__ == '__main__':
+    os.system('cls' if os.name == 'nt' else 'clear')
+    url_site, choice, choice_permanent = product_choice()
+    while True:
+        if choice_permanent == 'Nao':
+            url_site = url_site
+            main(url_site, choice)
+            # agendamento para rodar a cada 30 minutos
+            schedule.every(1).minutes.do(main(url_site, choice))
+            while True:
+                schedule.run_pending()
+                sleep(1)
+        elif choice_permanent == 'Sim':
+            main(url_site, choice)
+            # agendamento para rodar a cada 30 minutos usando while + sleep
+            sleep(1800)
+            url_site, choice, choice_permanent = product_choice()
